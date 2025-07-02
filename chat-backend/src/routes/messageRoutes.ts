f471ea25a8c9bee24d38c564b52294dc383d2a2f
rosjerry <rostomi1306@gmail.com>
2025-07-02 18:55:02 +0400

    fix: update commands to launch backend

--- chat-backend/src/routes/messageRoutes.ts
+++ chat-backend/src/routes/messageRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { receiveMessages, sendMessage } from '../controllers/messageController.js';
+import { receiveMessages, sendMessage } from '../controllers/messageController';
 const router = Router();
 
 router.get('/', receiveMessages);
