dc63ae75d4de0f1b455811812bb634ab30bfe980
rosjerry <rostomi1306@gmail.com>
2025-07-04 19:17:09 +0400

    feat: delete dist

--- chat-backend/dist/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messageController_js_1 = require("../controllers/messageController.js");
-const router = (0, express_1.Router)();
-router.get('/', messageController_js_1.receiveMessages);
-router.post('/', messageController_js_1.sendMessage);
-exports.default = router;
