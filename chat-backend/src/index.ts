b1e7f8879c168db216e969220f49b184a3176ce2
rosjerry <rostomi1306@gmail.com>
2025-07-05 13:11:08 +0400

    fix: remove lint, remove ws listener

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -3,5 +3,4 @@ import config from './config';
 
 server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
-  console.log(`WebSocket server is ready on ws://localhost:${config.port}`);
 });
